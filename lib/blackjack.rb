f95779dab3de689a5a5dd9b267b8680e6000d6bb
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2019-11-18 03:50:38 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -56,9 +56,9 @@ end
 
 def runner
   welcome
-  initial_round
+  card_total = initial_round
   until card_total > 21 do
-    hit?(card_total)
+    card_total = hit?(card_total)
     display_card_total(card_total)
   end
   end_game(card_total)
